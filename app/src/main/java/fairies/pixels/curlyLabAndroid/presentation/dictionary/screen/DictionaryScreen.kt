6b7450090aa76fb63dcf0d4ccc835031013e0313
Viktoria Dombrovskaya <spamovik123@yandex.ru>
2025-11-02 13:05:44 +0300

    Issue #4: Реализован экран словаря

--- app/src/main/java/fairies/pixels/curlyLabAndroid/presentation/dictionary/screen/DictionaryScreen.kt
+++ app/src/main/java/fairies/pixels/curlyLabAndroid/presentation/dictionary/screen/DictionaryScreen.kt
@@ -1,7 +1,80 @@
 package fairies.pixels.curlyLabAndroid.presentation.dictionary.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import fairies.pixels.curlyLabAndroid.presentation.dictionary.viewmodel.DictionaryViewModel
+import fairies.pixels.curlyLabAndroid.presentation.dictionary.viewmodel.Word
+import fairies.pixels.curlyLabAndroid.presentation.theme.DarkGreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DictionaryScreen(viewModel: DictionaryViewModel = viewModel()) {
+    var searchQuery by remember { mutableStateOf("") }
+
+    LaunchedEffect(searchQuery) {
+        viewModel.filterWords(searchQuery)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)
+    ) {
+
+        Text(
+            text = "Словарь", style = MaterialTheme.typography.displayLarge, color = DarkGreen
+        )
+
+        TextField(
+            value = searchQuery,
+            onValueChange = { newValue ->
+                searchQuery = newValue
+                viewModel.filterWords(newValue)
+            },
+            label = { Text("Поиск") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 8.dp)
+        )
+
+        LazyColumn {
+            items(viewModel.words.value) { word ->
+                WordItem(word)
+            }
+        }
+    }
+}
 
 @Composable
-fun DictionaryScreen() {
+fun WordItem(word: Word) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, bottom = 8.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = word.name, style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = word.description, style = MaterialTheme.typography.bodyMedium)
+        }
+    }
 }
