21795494cabf6a5c810438252a211e66bff93a14
Working Romario <lerome2002@gmal.com>
2025-11-29 22:34:30 +0300

    refactor

--- app/src/main/java/fairies/pixels/curlyLabAndroid/presentation/auth/viewmodel/SignInViewModel.kt
+++ app/src/main/java/fairies/pixels/curlyLabAndroid/presentation/auth/viewmodel/SignInViewModel.kt
@@ -56,23 +56,22 @@ class SignInViewModel @Inject constructor(
             return
         }
 
-//        val passwordValidation = validatePasswordUseCase.validatePasswordStrength(password.value)
-//        if (!passwordValidation.successful) {
-//            _errorMessage.value = passwordValidation.errorMessage
-//            return
-//        }
+        val passwordValidation = validatePasswordUseCase.validatePasswordStrength(password.value)
+        if (!passwordValidation.successful) {
+            _errorMessage.value = passwordValidation.errorMessage
+            return
+        }
 
         _isLoading.value = true
         viewModelScope.launch {
-//            val result = signInUseCase(email.value, password.value)
+            val result = signInUseCase(email.value, password.value)
             _isLoading.value = false
 
-//            if (result.isSuccess) {
-            if (true) {
+            if (result.isSuccess) {
                 _errorMessage.value = null
                 onSuccess()
             } else {
-//                _errorMessage.value = result.exceptionOrNull()?.message ?: "Ошибка входа"
+                _errorMessage.value = result.exceptionOrNull()?.message ?: "Ошибка входа"
             }
         }
     }
